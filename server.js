b351360eba520912499e15bb01250e756b736a08
Vishal V <vishal3967@gmail.com>
2017-08-23 19:59:43 +0530

    JS commit

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/article-one', function(req, res)	{
+	res.send('Article one served here');
+)};
+app.get('/article-two', function(req, res)	{
+	res.send('Article two served here');
+)};
+app.get('/article-three', function(req, res)	{
+	res.send('Article three served here');
+)};
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
