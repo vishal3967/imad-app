2f08434874c828f3c9e249460eef30cefedc6d02
Vishal V <vishal3967@gmail.com>
2017-08-27 20:43:53 +0530

    latest ui css&js changes

--- server.js
+++ server.js
@@ -5,6 +5,74 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+	title: 'Article One'
+	heading: 'Article One'
+	date: '27/8/17'
+	content:`   <p>
+					Content blah blah blah
+				</p>
+				<p>
+					More content blah blah blah
+				</p>`
+}
+var articleTwo = {
+	title: 'Article Two'
+	heading: 'Article Two'
+	date: '27/8/17'
+	content: `   <p>
+					Content blah blah blah
+				</p>
+				<p>
+					More content blah blah blah
+				</p>`
+}
+var articleThree = {
+	title: 'Article Three'
+	heading: 'Article Three'
+	date: '27/8/17'
+	content: `   <p>
+					Content blah blah blah
+				</p>
+				<p>
+					More content blah blah blah
+				</p>`
+}
+function createTemplate(data){
+	title = data.title;
+	heading = data.heading;
+	date = data.date;
+	content = data.content;
+var htmlTemplate = `
+	<html>
+		<head>
+			<link href="/ui/style.css" rel="stylesheet" />
+			<meta name="vieport" content = "width=device-width, intial-scale=1" />
+			<title>
+			${title}
+			</title>
+		</head>
+		<body>
+			<div class = "container">
+				<div>
+				<a href="/">Home</a>
+				</div>
+				<hr/>
+				<h4>
+				${heading}
+				</h4>
+				<div>
+					${date}
+				</div>
+				<div>
+					${content}
+				</div>
+			</div>
+		</body>			
+	</html>
+	`;
+	return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -17,13 +85,13 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 app.get('/article-one', function(req, res)	{
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/article-two', function(req, res)	{
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send(htmlTemplate(articleTwo));
 });
 app.get('/article-three', function(req, res)	{
-	res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+	res.send(htmlTemplate(articleThree));
 });
 
 
